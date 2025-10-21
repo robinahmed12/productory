d1338716da9c94d6c1d5b51d15a558e7193b0f53
robinahmed12 <shawonahmed0294@gmail.com>
2025-10-21 13:00:05 +0600

    initialize Angular project with CLI set up folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProductListComponent } from './features/products/product-list/product-list.component';
+import { ProductFormComponent } from './features/products/product-form/product-form.component';
+import { ProductDetailsComponent } from './features/products/product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ProductListComponent,
+    ProductFormComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
